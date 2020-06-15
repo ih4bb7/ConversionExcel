0339e98b5f0ba38f96382bb4cfda64665ec22744
小松 湧暉 <dtf20120919@yahoo.co.jp>
2020-06-15 23:49:37 +0900

    20200615

--- ConversionExcel/ConversionExcel/Controllers/HomeController.cs
+++ ConversionExcel/ConversionExcel/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.HtmlControls;
 
 namespace ConversionExcel.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string data)
         {
             return View();
+        }
+
+        public ActionResult btnAdd_Click()
+        {
+            var newdivs = new HtmlGenericControl("DIV");
+            newdivs.Attributes.Add("class", "maindivs");
+
+            //maindivs.Controls.Add(newdivs);
+            return View("Index");
         }
 
         public ActionResult About()
